4a885488e5dc24b81851e9a7c21cf13c9a8eba0c
Hylke <h.westerdijk@gmail.com>
2021-04-17 19:08:58 +0200

    Added basic video functionality

---
+++ Test2/Test2/DBConnect.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Test2
+{
+    public class DBConnect
+    {
+        string ConnectionString = @"data source=DESKTOP-4TP64DH;initial catalog=ChatrTestDB;trusted_connection=true;MultipleActiveResultSets=true";
+        SqlConnection con;
+
+        public SqlConnection getCon()
+        {
+            return con;
+        }
+
+
+        public void OpenConnection()
+        {
+            con = new SqlConnection(ConnectionString);
+            con.Open();
+        }
+
+
+        public void CloseConnection()
+        {
+            con.Close();
+        }
+
+
+        public void ExecuteQueries(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public SqlDataReader DataReader(string Query_)
+        {
+            SqlCommand cmd = new SqlCommand(Query_, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+
+        }
+
+
+        public object ShowDataInGridView(string Query_)
+        {
+            SqlDataAdapter dr = new SqlDataAdapter(Query_, ConnectionString);
+            DataSet ds = new DataSet();
+            dr.Fill(ds);
+            object dataum = ds.Tables[0];
+            return dataum;
+        }
+    }
+}
--- Test2/Test2/MainPage.xaml.cs
+++ Test2/Test2/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,14 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using VideoLibrary;
+using System.Security;
+using System.Security.Permissions;
+using Windows.UI.Popups;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Threading;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +32,58 @@ namespace Test2
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        DBConnect connection;
         public MainPage()
         {
             this.InitializeComponent();
+            setMediaPlayer();
+            
+            
+        }
+
+        private bool isPlaying()
+        {
+            if ( mediaPlayer.CurrentState.ToString() == "Playing") {
+                return true;               
+            } else {
+                return false;
+            }
+        }
+
+        public string GetTemporaryDirectory()
+        {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+            return tempDirectory;
+        }
+
+        public async void setMediaPlayer()
+        {
+            var video = await downloadVideoAsync("https://www.youtube.com/watch?v=QAUzWtLMnU0");
+            mediaPlayer.Source = new Uri(@"ms-appx:///" + video);
+
+            mediaPlayer.Play();
+        }
+
+        public string downloadVideo(string link)
+        {
+            var path = GetTemporaryDirectory() + "/test.mp4";
+            var youTube = YouTube.Default; // starting point for YouTube actions
+            var video = youTube.GetVideo(link); // gets a Video object with info about the video
+            
+            File.WriteAllBytes(path, video.GetBytes());
+
+            return path;
+        }
+
+        public async Task<string> downloadVideoAsync(string link)
+        {
+            return await Task.Run(() => downloadVideo(link));
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(isPlaying());
         }
     }
 }
